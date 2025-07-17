723e122a914a3b4025b8dd12fa058cdd0b49b767
TheStranjer <791672+TheStranjer@users.noreply.github.com>
2025-07-17 06:53:37 -0600

    Scope, not world

--- module/settings.js
+++ module/settings.js
@@ -2,7 +2,7 @@ export const registerSystemSettings = () => {
   game.settings.register("god-machine", "apiKey", {
     name: game.i18n.format("god-machine.api-key-title"),
     hint: game.i18n.format("god-machine.api-key-hint"),
-    scope: "world",
+    scope: "client",
     config: true,
     type: String,
     default: "",
@@ -12,7 +12,7 @@ export const registerSystemSettings = () => {
   game.settings.register("god-machine", "model", {
     name: game.i18n.format("god-machine.model-title"),
     hint: game.i18n.format("god-machine.model-hint"),
-    scope: "world",
+    scope: "client",
     config: true,
     type: String,
     default: "gpt-4o",
@@ -22,7 +22,7 @@ export const registerSystemSettings = () => {
   game.settings.register("god-machine", "endpoint", {
     name:  game.i18n.format("god-machine.endpoint-title"),
     hint: game.i18n.format("god-machine.endpoint-hint"),
-    scope: "world",
+    scope: "client",
     config: true,
     type: String,
     default: "https://api.openai.com/v1/chat/completions",
